018af0b230b8e3c6f0c21403e8b3670c82cd7ccd
lasha-gamrekelashvili <lash.gamrekelashvili@gmail.com>
2022-12-17 01:41:49 +0400

    initial commit

---
+++ src/main/java/com/example/nubesexercitatio/NubesExercitatioApplication.java
@@ -0,0 +1,13 @@
+package com.example.nubesexercitatio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NubesExercitatioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NubesExercitatioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/nubesexercitatio/NubesExercitatioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nubesexercitatio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NubesExercitatioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
