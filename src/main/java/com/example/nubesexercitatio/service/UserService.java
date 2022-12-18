8d65f570f33f00de3603bb8acfac03ceb63e3edc
lasha-gamrekelashvili <lash.gamrekelashvili@gmail.com>
2022-12-19 01:57:39 +0400

    changed port numbers

--- src/main/java/com/example/nubesexercitatio/service/UserService.java
+++ src/main/java/com/example/nubesexercitatio/service/UserService.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class UserService {
-
     private final UserRepository userRepository;
 
     public UserEntity saveUser(UserEntity userEntity){
