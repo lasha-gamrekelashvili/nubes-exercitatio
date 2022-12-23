49fede57b1d4cc4628ff401d9c4e53c2137f7c30
lasha-gamrekelashvili <lash.gamrekelashvili@gmail.com>
2022-12-23 17:47:46 +0400

    test

---
+++ src/main/java/com/example/nubesexercitatio/controller/HealthcheckController.java
@@ -0,0 +1,18 @@
+package com.example.nubesexercitatio.controller;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/healthcheck")
+public class HealthcheckController {
+
+    @GetMapping("")
+    public ResponseEntity<String> healthcheck(){
+        return new ResponseEntity<>("Bozebi xart", HttpStatus.OK);
+    }
+}
