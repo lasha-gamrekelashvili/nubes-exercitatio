c96784dcd015d15addbfe0513a15861caf36337c
lasha-gamrekelashvili <lash.gamrekelashvili@gmail.com>
2022-12-31 15:12:44 +0400

    rame

--- src/main/resources/db/migration/V1__user.sql
+++ src/main/resources/db/migration/V1__user.sql
@@ -4,4 +4,11 @@ CREATE TABLE `user`
     first_name varchar(255) NOT NULL,
     last_name  varchar(255) NOT NULL,
     PRIMARY KEY (id)
+);
+
+CREATE TABLE question
+(
+    id        bigint(20) NOT NULL AUTO_INCREMENT,
+    question_text varchar(255) NOT NULL,
+    PRIMARY KEY (id)
 )
